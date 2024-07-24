455dcbe05c809b39fda2e0bd282cc56f262910cb
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-07-24 20:51:10 +0900

    base setting

---
+++ src/main/java/beyond/ordersystem/member/Member.java
@@ -0,0 +1,13 @@
+package beyond.ordersystem.member;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
